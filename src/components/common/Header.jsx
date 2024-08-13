5119f47c6175ae4d345db712f5b80315f09cd007
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-13 23:18:23 +0100

    add register feature, hero and footer to page

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,7 +2,14 @@ import { Flex } from "@chakra-ui/react";
 
 export default function Header({ children }) {
   return (
-    <Flex alignItems={"center"} height={"72px"} position={"sticky"} top={0}>
+    <Flex
+      alignItems={"center"}
+      height={"72px"}
+      position={"sticky"}
+      top={0}
+      bg={"gray.800"}
+      as={"header"}
+    >
       {children}
     </Flex>
   );
--- src/routes/page.jsx
+++ src/routes/page.jsx
@@ -2,6 +2,10 @@ import Container from "@/components/common/Container";
 import Logo from "@/components/common/Logo";
 import Header from "@/components/common/Header";
 import LoginBtn from "@/features/auth/login/components/LoginBtn";
+import RegisterBtn from "@/features/auth/register/components/RegisterBtn";
+import { Center } from "@chakra-ui/react";
+import Hero from "@/components/common/Hero";
+import Authorship from "@/components/common/Authorship";
 
 export default function IndexPage() {
   return (
@@ -9,7 +13,14 @@ export default function IndexPage() {
       <Header>
         <Logo to="/" />
         <LoginBtn ml="auto" />
+        <RegisterBtn ml={6} />
       </Header>
+      <Center flex={1} mt={12}>
+        <Hero />
+      </Center>
+      <Center py={16}>
+        <Authorship />
+      </Center>
     </Container>
   );
 }
