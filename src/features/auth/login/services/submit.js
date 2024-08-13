9021badbb4904d8ec20ff3b0986ad9c5ea887e32
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-13 23:29:48 +0100

    add v1 to api route

--- src/features/auth/login/services/submit.js
+++ src/features/auth/login/services/submit.js
@@ -1,6 +1,6 @@
 export default function getSubmit(form) {
   return async () =>
-    fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+    fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/login`, {
       method: "POST",
       body: JSON.stringify(form.getValues()),
       headers: {
