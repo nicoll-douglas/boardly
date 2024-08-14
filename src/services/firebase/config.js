3fd72be66a023b92d2c8813ccf35890302050644
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-14 19:27:19 +0100

    file: move config to services/firebase

---
+++ src/services/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDW3O9dYkQS3TpU9u1i73maCbqi4ffZSVI",
+  authDomain: "rum-c5d24.firebaseapp.com",
+  projectId: "rum-c5d24",
+  storageBucket: "rum-c5d24.appspot.com",
+  messagingSenderId: "754343954480",
+  appId: "1:754343954480:web:e0aec06dc0ed675cdf45f5",
+};
+
+export default firebaseConfig;
