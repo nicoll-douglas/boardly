c43f855880283b82f8e502fc6edb44b1684bc4a6
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-15 17:51:12 +0100

    file: rename

---
+++ src/routes/auth/reset/page.jsx
@@ -0,0 +1,13 @@
+import FormModal from "@/components/common/FormModal";
+import NewPwdForm from "@/features/auth/passwordReset/components/NewPwdForm";
+import { useParams } from "react-router-dom";
+
+export default function NewPwdPage() {
+  const { token } = useParams();
+
+  return (
+    <FormModal isOpen={true} motionPreset="none">
+      <NewPwdForm token={token} />
+    </FormModal>
+  );
+}
