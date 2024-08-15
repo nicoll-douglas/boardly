31c8c10a43143eaf3dbf9acf86a3000aaf27552a
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-15 14:55:54 +0100

    test: verification page tests sucessful

--- cypress/e2e/features/auth/emailVerification/verification.cy.js
+++ cypress/e2e/features/auth/emailVerification/verification.cy.js
@@ -1,3 +1,63 @@
 describe("Email verification page", () => {
-  it("Should display ");
+  const token = "token";
+
+  beforeEach(() => {
+    cy.visit(`/auth/verify/${token}`);
+  });
+
+  it("Should display UI feedback on 401 response", () => {
+    cy.intercept("PATCH", "**/api/auth/verify", (req) => {
+      req.reply({
+        statusCode: 401,
+      });
+    }).as("fetch");
+    cy.wait("@fetch");
+    cy.contains("Failed to Verify");
+    cy.contains(
+      "We could not verify your email address. The link is either invalid or expired."
+    );
+  });
+
+  it("Should display UI feedback on 500 response", () => {
+    cy.intercept("PATCH", "**/api/auth/verify", (req) => {
+      req.reply({
+        statusCode: 500,
+      });
+    }).as("fetch");
+    cy.wait("@fetch");
+    cy.contains("Server Error");
+    cy.contains("Something went wrong, please try again later.");
+  });
+
+  it("Should display UI feedback on 429", () => {
+    cy.intercept("PATCH", "**/api/auth/verify", (req) => {
+      req.reply({
+        statusCode: 429,
+      });
+    }).as("fetch");
+    cy.wait("@fetch");
+    cy.contains("Too Many Requests");
+    cy.contains("Please try again in 15 minutes.");
+  });
+
+  it("Should display UI feedback on 200 and navigates to home", () => {
+    const accessToken = "accessToken";
+
+    cy.intercept("PATCH", "**/api/auth/verify", (req) => {
+      req.reply({
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          accessToken,
+        },
+      });
+    }).as("fetch");
+    cy.wait("@fetch");
+    cy.contains("Email Verified");
+    cy.contains("You will be redirected shortly, welcome to Lorem!");
+    cy.wait(3000);
+    cy.url().should("include", "/home");
+  });
 });
