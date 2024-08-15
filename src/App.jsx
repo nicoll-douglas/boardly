e18920ace42d3bdfab2a1f6ac16dc408b4583b24
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-15 16:21:08 +0100

    feat: complete verification page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import "./assets/styles.css";
 import IndexPage from "./routes/page";
 import HomePage from "./routes/home/page";
+import VerificationPage from "./routes/auth/verify/page";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />}></Route>
       <Route path="/home" element={<HomePage />}></Route>
+      <Route path="/auth/verify/:token" element={<VerificationPage />}></Route>
     </Routes>
   );
 }
--- src/Contexts.jsx
+++ src/Contexts.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/index";
 import { AuthProvider } from "./lib/contexts/AuthContext";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function Contexts({ children }) {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
-      </AuthProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <AuthProvider>
+          <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
