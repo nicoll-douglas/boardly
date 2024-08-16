f04126e67a2163143edb05f7b55ac47371c41602
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-16 13:10:53 +0100

    implement hook to handle protected data queries

--- src/lib/hooks/useProtectedQuery.js
+++ src/lib/hooks/useProtectedQuery.js
@@ -1,10 +1,21 @@
 import { useToast } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { networkError, serverError, tooMany } from "@/lib/constants/toasts";
+import { useEffect, useState } from "react";
+import {
+  networkError,
+  serverError,
+  tooMany,
+  unauthorized,
+  genericError,
+} from "@/lib/constants/toasts";
+import { useNavigate } from "react-router-dom";
+import useAuth from "@/lib/hooks/useAuth";
 
-export default function useProtected(query) {
+export default function useProtectedQuery(query) {
   const { data, isLoading, isError } = query;
   const toast = useToast();
+  const navigate = useNavigate();
+  const { setAccessToken } = useAuth();
+  const [protectedData, setProtectedData] = useState(null);
 
   useEffect(() => {
     if (isError) toast(networkError);
@@ -12,6 +23,28 @@ export default function useProtected(query) {
 
   useEffect(() => {
     if (!data) return;
-    if (data.bodyUsed) return;
+
+    switch (data.status) {
+      case 401:
+        toast(unauthorized);
+        setTimeout(navigate, 250, "/");
+        break;
+      case 500:
+        toast(serverError);
+        break;
+      case 429:
+        toast(tooMany);
+        break;
+      case 200:
+        data
+          .json()
+          .then(({ accessToken, ...rest }) => {
+            if (accessToken) setAccessToken();
+            setProtectedData(rest);
+          })
+          .catch(() => toast(genericError));
+    }
   }, [data]);
+
+  return { isLoading, protectedData };
 }
