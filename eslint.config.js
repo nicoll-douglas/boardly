147fafc5bc35714f1e6f9c936fed402a0965fad6
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-16 10:41:10 +0100

    refactor: move toggleable form feat to home page

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,6 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
-        ...cypress.configs.globals,
       },
       parserOptions: {
         ecmaVersion: "latest",
--- src/routes/home/page.jsx
+++ src/routes/home/page.jsx
@@ -1,1 +1,13 @@
-export default function HomePage() {}
+import Container from "@/components/common/Container";
+import Header from "@/components/common/Header";
+import Logo from "@/components/common/Logo";
+
+export default function HomePage() {
+  return (
+    <Container>
+      <Header>
+        <Logo to="/home" />
+      </Header>
+    </Container>
+  );
+}
