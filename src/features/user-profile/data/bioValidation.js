67c8bf3246029af3ae310ad2550339ccb215163a
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-20 18:54:44 +0100

    implement regex for profile bio submissions

--- src/features/user-profile/data/bioValidation.js
+++ src/features/user-profile/data/bioValidation.js
@@ -1,9 +1,13 @@
+import { laxAndWhitespace } from "@/lib/constants/regex";
+
 const validation = {
   required: false,
   maxLength: {
     value: 100,
     message: "Bio must be no more than 100 characters long",
   },
+  validate: (value) =>
+    laxAndWhitespace.test(value) || "Bio contains an invalid character",
 };
 
 export default validation;
