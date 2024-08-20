b6200a532226089a26488bde00c3250385b7267b
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-21 00:06:44 +0100

    deps: install multer

--- src/services/firebase-storage.js
+++ src/services/firebase-storage.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT_KEY),
+  credential: admin.credential.cert(serviceAccount),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
-const storage = admin.storage().bucket();
+const bucket = admin.storage().bucket();
 
-module.exports = storage;
+module.exports = bucket;
