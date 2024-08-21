9fee552d525f928f9c75666127cf0e314f507e12
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-21 15:43:37 +0100

    file: add image upload config

---
+++ src/config/imgUploads.js
@@ -0,0 +1,4 @@
+const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
+const ALLOWED_TYPES = ["image/jpeg", "image/png"];
+
+module.exports = { MAX_FILE_SIZE, ALLOWED_TYPES };
--- src/middleware/common/avatarUpload.js
+++ src/middleware/common/avatarUpload.js
@@ -1,9 +1,10 @@
 const multer = require("multer");
+const { MAX_FILE_SIZE } = require("@/config/imgUploads");
 
 const storage = multer.memoryStorage();
 const processAvatar = multer({
   storage: storage,
-  limits: { fileSize: 2 * 1024 * 1024 },
+  limits: { fileSize: MAX_FILE_SIZE, files: 1, fields: 3 },
 }).single("avatar");
 
 module.exports = processAvatar;
