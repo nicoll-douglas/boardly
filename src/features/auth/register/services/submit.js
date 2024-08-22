14e1b16eb931163f75f11663aee211714fb26e1c
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-22 13:53:19 +0100

    fix: typo in URLs

--- src/features/auth/register/services/submit.js
+++ src/features/auth/register/services/submit.js
@@ -1,6 +1,6 @@
 export default function getSubmit(form) {
   return async () =>
-    fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/register`, {
+    fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
