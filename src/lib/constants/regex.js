80fd4bdbddfdb45034ed79f0b48b5f0c9f505cbe
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-22 15:14:21 +0100

    fix: validation and regexes to allow null values

--- src/lib/constants/regex.js
+++ src/lib/constants/regex.js
@@ -1,6 +1,6 @@
-const lax = /^[A-Za-z0-9!@#$%^&*()\-_=+[\]{}|;:'",.<>?/~]+$/;
-const restricted = /^[a-zA-Z0-9_-]+$/;
-const laxAndWhitespace = /^[A-Za-z0-9!@#$%^&*()\-_=+[\]{}|;:'",.<>?/~\s]+$/;
+const lax = /^[A-Za-z0-9!@#$%^&*()\-_=+[\]{}|;:'",.<>?/~]*$/;
+const restricted = /^[a-zA-Z0-9_-]*$/;
+const laxAndWhitespace = /^[A-Za-z0-9!@#$%^&*()\-_=+[\]{}|;:'",.<>?/~\s]*$/;
 
 module.exports = {
   lax,
--- src/lib/validationSchemas/profile.js
+++ src/lib/validationSchemas/profile.js
@@ -2,13 +2,9 @@ const Joi = require("joi");
 const { laxAndWhitespace } = require("@/lib/constants/regex");
 
 const profileSchema = {
-  age: Joi.number().min(13).max(120),
-  bio: Joi.string().max(100).pattern(laxAndWhitespace),
-  pronouns: Joi.string().custom((value, helpers) => {
-    if (value === undefined) return;
-    const values = ["he/him", "she/her", "they/them", "none"];
-    if (!values.includes(value)) return helpers.error("any.invalid");
-  }),
+  age: Joi.number().min(13).max(120).allow(null),
+  bio: Joi.string().max(100).pattern(laxAndWhitespace).allow(""),
+  pronouns: Joi.string().valid("", "he/him", "she/her", "they/them"),
 };
 
 module.exports = profileSchema;
