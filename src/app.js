6d9decfd47467c01e3709f2ce2ed8ee21bbf06a8
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-26 15:37:59 +0100

    refactor: protected route handling to main app entry

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const customMethods = require("./middleware/common/customMethods");
 const errorHandler = require("./middleware/common/errorHandler");
 const notFoundHandler = require("./middleware/common/notFoundHandler");
 const logger = require("@/middleware/logging/winston");
+const limiter = require("@/middleware/common/limiter");
+const validateHTTPAuth = require("@/middleware/validation/validateHTTPAuth");
+const verifyAuth = require("@/middleware/auth/verifyAuth");
 
 const app = express();
 app.use("/", (req, res, next) => {
@@ -20,7 +23,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(customMethods);
 app.use("/api/auth", require("./routers/auth"));
+
+// protected routes
+app.use(limiter(100, 0.6));
+app.use(validateHTTPAuth);
+app.use(verifyAuth);
 app.use("/api/me", require("./routers/me"));
+
 app.use(notFoundHandler);
 app.use(errorHandler);
 
--- src/routers/me.js
+++ src/routers/me.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const limiter = require("@/middleware/common/limiter");
-const validateHTTPAuth = require("@/middleware/validation/validateHTTPAuth");
-const verifyAuth = require("@/middleware/auth/verifyAuth");
 const processImg = require("@/middleware/common/imgUploads");
 const validateBody = require("@/middleware/validation/validateBody");
 const profileSchema = require("@/lib/validationSchemas/profile");
@@ -9,7 +6,6 @@ const validateImg = require("@/middleware/validation/validateImg");
 
 const router = express.Router();
 
-router.use("/", limiter(100, 0.6), validateHTTPAuth, verifyAuth);
 router.get("/", require("@/controllers/me/get"));
 router.post(
   "/profile/info",
