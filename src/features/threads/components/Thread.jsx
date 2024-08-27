2f65ecbe4a32704a5461273bdb843fe823033928
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-27 15:41:04 +0100

    file: add new

---
+++ src/features/threads/components/Thread.jsx
@@ -0,0 +1,5 @@
+import ThreadPost from "./ThreadPost";
+
+export default function Thread() {
+  return <ThreadPost></ThreadPost>;
+}
---
+++ src/features/threads/components/ThreadPost.jsx
@@ -0,0 +1,9 @@
+import { Card, CardHeader } from "@chakra-ui/react";
+
+export default function ThreadPost() {
+  return (
+    <Card h={24} w={"full"}>
+      <CardHeader></CardHeader>
+    </Card>
+  );
+}
