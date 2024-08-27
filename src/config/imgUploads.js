b1ac518e568c19d2c50c6dce6e251d1fdba0dc89
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-27 02:35:52 +0100

    fix: typo

--- src/config/imgUploads.js
+++ src/config/imgUploads.js
@@ -1,4 +1,4 @@
-const MAX_FILE_SIZE = 2 * 1025 * 1024; // 2MB
+const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
 const ALLOWED_TYPES = ["image/jpeg", "image/png"];
 const MAX_DIMENSIONS = { width: 500, height: 500 };
 
