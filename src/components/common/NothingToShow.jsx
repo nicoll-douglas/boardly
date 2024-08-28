db58a644eb8bc6741d8f3263dd586a52e95a1574
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-28 14:51:34 +0100

    file: add new

---
+++ src/components/common/NothingToShow.jsx
@@ -0,0 +1,9 @@
+import { Tag } from "@chakra-ui/react";
+
+export default function NothingToShow() {
+  return (
+    <Tag maxW={"fit-content"} mx={"auto"}>
+      Nothing to show :(
+    </Tag>
+  );
+}
