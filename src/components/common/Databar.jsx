8c5880556c7e5afe2ac3a03c466b95e87a446412
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-28 15:44:12 +0100

    style: edit

--- src/components/common/Databar.jsx
+++ src/components/common/Databar.jsx
@@ -1,14 +1,17 @@
 import { Flex, Divider, Heading, Box } from "@chakra-ui/react";
+import noWrap from "@/lib/constants/noWrap";
 
 export default function DataBar({ name, value, dividerTop, dividerBottom }) {
   return (
     <>
       {dividerTop && <Divider />}
-      <Flex justifyContent={"space-between"} my={1} gap={4}>
+      <Flex justifyContent={"space-between"} my={1} gap={8}>
         <Heading size={"sm"} color={"gray.300"}>
           {name}
         </Heading>
-        <Box textAlign={"right"}>{value}</Box>
+        <Box textAlign={"right"} fontSize={"sm"} {...noWrap}>
+          {value}
+        </Box>
       </Flex>
       {dividerBottom && <Divider />}
     </>
--- src/features/boards/components/ThreadsList.jsx
+++ src/features/boards/components/ThreadsList.jsx
@@ -3,11 +3,12 @@ import {
   CardHeader,
   CardBody,
   Heading,
-  SkeletonText,
-  Tag,
+  Skeleton,
+  Flex,
 } from "@chakra-ui/react";
 import ThreadLink from "./ThreadLink";
 import useBoard from "../hooks/useBoard";
+import NothingToShow from "@/components/common/NothingToShow";
 
 export default function ThreadsList() {
   const { isLoading, board } = useBoard();
@@ -21,23 +22,24 @@ export default function ThreadsList() {
         <Heading size={"sm"}>Threads</Heading>
       </CardHeader>
       <CardBody p={4} pt={0} display={"flex"} flexDir={"column"}>
-        <SkeletonText noOfLines={12} isLoaded={isLoaded}>
-          {length > 0 &&
-            threads.map(({ _id, title, author }) => (
-              <ThreadLink
-                key={_id}
-                threadName={title}
-                authorName={author.username}
-                authorLink={`/users/${author._id}`}
-                link={`/boards/${board.name}/threads/${_id}`}
-              />
+        {length > 0 &&
+          threads.map(({ _id, title, author }) => (
+            <ThreadLink
+              key={_id}
+              threadName={title}
+              authorName={author.username}
+              authorLink={`/users/${author._id}`}
+              link={`/boards/${board.name}/threads/${_id}`}
+            />
+          ))}
+        {length === 0 && <NothingToShow />}
+        {length < 0 && (
+          <Flex flexDir={"column"} gap={2}>
+            {Array.from({ length: 6 }, (_, index) => (
+              <Skeleton key={index} isLoaded={isLoaded} h={6} />
             ))}
-          {length === 0 && (
-            <Tag maxW={"fit-content"} mx={"auto"}>
-              Nothing to show :(
-            </Tag>
-          )}
-        </SkeletonText>
+          </Flex>
+        )}
       </CardBody>
     </Card>
   );
