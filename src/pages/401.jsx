1100794e71326c70f4ce44e0fc6683557fc65e76
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-09 21:57:16 +0100

    file/feat: add 401 component

---
+++ src/pages/401.jsx
@@ -0,0 +1,12 @@
+import authUrl from "@/assets/auth.svg";
+import OffPage from "@/components/common/OffPage";
+
+export default function Unauthorized() {
+  return (
+    <OffPage
+      title="Unauthorized"
+      message="Please login to continue or access the requested resource."
+      imageUrl={authUrl}
+    />
+  );
+}
