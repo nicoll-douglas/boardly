1efcf43aa708c9803380b504e3e0f61c0ba3740e
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-09 19:11:11 +0100

    file: rename/move

--- src/validation/profile.schema.js
+++ src/validation/profile.schema.js
@@ -1,10 +1,10 @@
 const Joi = require("joi");
 const { laxAndWhitespace } = require("@/lib/constants/regex");
 
-const profileSchema = {
+const profileSchema = Joi.object({
   age: Joi.number().min(13).max(120).allow(null),
   bio: Joi.string().max(100).pattern(laxAndWhitespace).allow(""),
   pronouns: Joi.string().valid("", "he/him", "she/her", "they/them"),
-};
+});
 
 module.exports = profileSchema;
