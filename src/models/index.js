6a92e997acb31c79cad8a7e36000c2ac99612c75
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-09 23:05:00 +0100

    file: delete

--- src/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require("./User");
-const Thread = require("./Thread");
-const Reply = require("./Reply");
-const Board = require("./Board");
-
-module.exports = { User, Thread, Reply, Board };
--- src/routers/boards.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/:boardName", require("@/controllers/boards/get"));
-
-module.exports = router;
