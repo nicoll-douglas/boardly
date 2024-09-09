393a207396637c16e27384f4c40fdb0ba100daa2
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-09 19:09:19 +0100

    file: rename

--- src/middleware/common/handleImage.js
+++ src/middleware/common/handleImage.js
@@ -1,12 +1,10 @@
 const multer = require("multer");
 const { MAX_FILE_SIZE } = require("@/config/imgUploads");
 
-function processImg(fieldName, limits = {}) {
+exports.one = (fieldName) => {
   const storage = multer.memoryStorage();
   return multer({
     storage: storage,
-    limits: { fileSize: MAX_FILE_SIZE, files: 1, ...limits },
+    limits: { fileSize: MAX_FILE_SIZE, files: 1 },
   }).single(fieldName);
-}
-
-module.exports = processImg;
+};
