77d55aa002e7bca5526476ea7ea53658c1db2b41
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-10 13:23:01 +0100

    add users route

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@ import NotFound from "./pages/404";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import Optimistic from "./components/special/Optimistic";
+import User from "./pages/users/[username]/index";
+
+import ServerError from "./pages/500";
+import Unauthorized from "./pages/401";
+import TooMany from "./pages/429";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
+      <Route path="*" element={<NotFound />} />
+
       <Route
         path="/auth/verify"
         element={
@@ -25,7 +32,12 @@ function App() {
       <Route path="/auth/reset" element={<Reset />} />
       <Route path="/auth/login" element={<Login />} />
       <Route path="/auth/register" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
+
+      <Route path="/users/:username" element={<User />} />
+
+      <Route path="/429" element={<TooMany />} />
+      <Route path="/500" element={<ServerError />} />
+      <Route path="/401" element={<Unauthorized />} />
     </Routes>
   );
 }
