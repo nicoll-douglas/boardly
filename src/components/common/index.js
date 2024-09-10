2033709bc6ff74329e450249dec93cfe9d3af98c
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-10 22:18:46 +0100

    file: add components index

---
+++ src/components/common/index.js
@@ -0,0 +1,11 @@
+export { default as Authorship } from "./Authorship";
+export { default as Container } from "./Container";
+export { default as FormModal } from "./FormModal";
+export { default as Grid } from "./Grid";
+export { default as Header } from "./Header";
+export { default as Hero } from "./Hero";
+export { default as Loader } from "./Loader";
+export { default as Logo } from "./Logo";
+export { default as NothingToShow } from "./NothingToShow";
+export { default as OffPage } from "./OffPage";
+export { default as Separator } from "./Separator";
