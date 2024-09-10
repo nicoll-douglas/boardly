28ea4c612cacc735400d37b846a74ba97e4f2c3b
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-10 23:53:25 +0100

    implement threads tab in profile

--- src/features/user-profile/components/ThreadsTab.jsx
+++ src/features/user-profile/components/ThreadsTab.jsx
@@ -1,1 +1,27 @@
-export default function
+import { Center, TabPanel, Text, VStack } from "@chakra-ui/react";
+import useProfile from "../hooks/useProfile";
+import { NothingToShow } from "@/components/common";
+import ThreadPreview from "./ThreadPreview";
+
+export default function ThreadsTab() {
+  const data = useProfile();
+  const threads = data.profile.threads;
+
+  return (
+    <TabPanel>
+      <VStack gap={4}>
+        {threads.length === 0 ? (
+          <Center w={"full"}>
+            <NothingToShow>
+              <Text>Create a thread to get started!</Text>
+            </NothingToShow>
+          </Center>
+        ) : (
+          threads.map((thread) => (
+            <ThreadPreview key={thread._id} thread={thread} />
+          ))
+        )}
+      </VStack>
+    </TabPanel>
+  );
+}
