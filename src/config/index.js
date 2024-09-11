272757886818e7316d62d8d8a2eb2b716889cf38
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-11 16:25:56 +0100

    file: add combined config file

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  corsOptions: {
+    origin: process.env.HTTP_CLIENT,
+    credentials: true,
+    optionsSuccessStatus: 200,
+  },
+  imgUploads: {
+    maxSize: 2 * 1024 * 1024,
+    allowedTypes: ["image/jpeg", "image/png"],
+  },
+  JWT: {
+    accessDuration: 5 * 60 * 1000,
+    refreshDuration: 28 * 24 * 60 * 60 * 1000,
+    emailVerificationDuration: 24 * 60 * 60 * 1000,
+    passwordResetDuration: 60 * 60 * 1000,
+  },
+  userRoles: {
+    basic: 0,
+    self: 1,
+    admin: 2,
+  },
+};
