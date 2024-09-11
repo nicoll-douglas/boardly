0674d31095571d6e783f829c9adc4109a7d574ad
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-11 13:51:36 +0100

    file: move global contexts to new file, add contexts barrel

--- src/contexts/GlobalContexts.jsx
+++ src/contexts/GlobalContexts.jsx
@@ -1,18 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme/index";
-import { AuthProvider } from "./lib/contexts/AuthContext";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import theme from "@/theme";
 
 const queryClient = new QueryClient();
 
-export default function Contexts({ children }) {
+export default function GlobalContexts({ children }) {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <ChakraProvider theme={theme}>{children}</ChakraProvider>
-        </AuthProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </QueryClientProvider>
     </BrowserRouter>
   );
---
+++ src/contexts/index.js
@@ -0,0 +1,1 @@
+export { default as GlobalContexts } from "./GlobalContexts";
