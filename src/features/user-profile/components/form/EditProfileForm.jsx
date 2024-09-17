741ca67bdb409cd7e07af405d5431faed000025b
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-17 23:01:33 +0100

    file: delete

--- src/features/user-profile/components/form/EditProfileForm.jsx
+++
@@ -1,7 +0,0 @@
-import { useForm } from "react-hook-form";
-import useProfile from "../../hooks/useProfile";
-
-export default function EditProfileForm() {
-  const { data } = useProfile();
-  const form = useForm({ shouldUnregister: true });
-}
