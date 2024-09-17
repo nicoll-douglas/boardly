91c0b3d195176df438d7da9e4aaa59da0d623205
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-17 15:43:16 +0100

    fix: typo

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -8,13 +8,13 @@ module.exports = async (req, res, next) => {
     const usernameTaken = await User.findOne({ username });
     if (usernameTaken) {
       req.log("username, 409, sent");
-      return res.status(409).feedback(["username", "Username is taken"]);
+      return res.status(409)._feedback(["username", "Username is taken"]);
     }
 
     const emailTaken = await User.findOne({ email });
     if (emailTaken) {
       req.log("email, 409, sent");
-      return res.status(409).feedback(["email", "Email is already in use"]);
+      return res.status(409)._feedback(["email", "Email is already in use"]);
     }
 
     const newUser = new User({ email, username });
