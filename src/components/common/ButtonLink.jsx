2f65374ca3c48c7334b2ad87d8e5bfa440a9b8b4
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-25 18:26:57 +0100

    fix: responsiveness

--- src/components/common/ButtonLink.jsx
+++ src/components/common/ButtonLink.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function ButtonLink({ to, children, bold, overlay, ...rest }) {
   return overlay ? (
-    <LinkOverlay as={Link} to={to}>
+    <LinkOverlay as={Link} to={to} maxW="full">
       <ButtonLinkStyled bold={bold} {...rest}>
         {children}
       </ButtonLinkStyled>
@@ -20,9 +20,9 @@ function ButtonLinkStyled({ bold, children, ...rest }) {
   return (
     <Button
       variant={"link"}
-      maxW={"fit-content"}
       py={1}
       h={8}
+      maxW={"full"}
       borderRadius={0}
       fontWeight={bold ? 600 : 400}
       {...noWrap}
