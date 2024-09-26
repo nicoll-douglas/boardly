5030d906eed9cdd06ca3976714f50f0e80a3a7bf
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-09-26 10:05:13 +0100

    update constants

--- src/lib/constants/regex.js
+++ src/lib/constants/regex.js
@@ -3,7 +3,7 @@ const regex = {
     noWhiteSpace: /^[A-Za-z0-9!@#$%^&*()\-_=+[\]{}|;:'",.<>?/~]*$/,
     whiteSpace: /^[A-Za-z0-9!@#$%^&*()\-_=+[\]{}|;:'",.<>?/~\s]*$/,
   },
-  restricted: /^[a-zA-Z0-9_-]*$/,
+  restricted: /^[a-zA-Z0-9_.-]*$/,
 };
 
 export default regex;
