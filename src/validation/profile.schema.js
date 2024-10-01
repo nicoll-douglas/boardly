22a04e8cea9bedf854b5306e3a5d281ea557d709
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-01 17:23:13 +0100

    fix: typo / incorrect validation

--- src/validation/profile.schema.js
+++ src/validation/profile.schema.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const profileSchema = Joi.object({
   age: Joi.alternatives().try(
     Joi.number().max(120).min(0).optional(),
-    Joi.string().valid("").optional()
+    Joi.string().allow("").optional()
   ),
   pronouns: Joi.string().max(10).allow("").optional(),
   bio: Joi.string().max(100).allow("").optional(),
--- src/validation/thread.schema.js
+++ src/validation/thread.schema.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const threadSchema = Joi.object({
   board: Joi.string().required(),
   title: Joi.string().max(50).required(),
-  body: Joi.string().max(100).valid("").optional(),
-});
+  body: Joi.string().max(100).allow("").optional(),
+}).unknown();
 
 module.exports = threadSchema;
