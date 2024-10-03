3bdddccf7fcc139fec35cfd10dd5604732314409
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-03 19:16:53 +0100

    add links to alternate pages

--- src/features/auth/components/Optimistic.jsx
+++ src/features/auth/components/Optimistic.jsx
@@ -1,11 +1,11 @@
 import usePrivilege from "../hooks/usePrivilege";
 import { Navigate } from "react-router-dom";
-import { Loader } from "@/components/common";
+import { DelayFallback } from "@/components/common";
 
 export default function Optimistic({ redirect = "/home", children }) {
   const { elevated, isLoading } = usePrivilege();
 
-  if (isLoading) return <Loader />;
+  if (isLoading) return <DelayFallback />;
   if (elevated) return <Navigate to={redirect} />;
 
   return children;
