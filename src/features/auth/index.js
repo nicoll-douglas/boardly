5de93748e9a72db94bf7df131441742936daca77
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-04 16:30:40 +0100

    fix: typo

--- src/features/auth/index.js
+++ src/features/auth/index.js
@@ -1,7 +1,7 @@
 export { default as ForgotPwdForm } from "./components/ForgotPwdForm";
 export { default as LoginBtn } from "./components/LoginBtn";
 export { default as LoginForm } from "./components/LoginForm";
-export { default as NewPwdForm } from "./components/NewPwdForm";
+export { default as NewPwdForm } from "./components/newPwdForm";
 export { default as RegisterBtn } from "./components/RegisterBtn";
 export { default as RegisterForm } from "./components/RegisterForm";
 export { default as Verification } from "./components/Verification";
