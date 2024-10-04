b24d351b92fcbdacc4686cc63d366b812583ad14
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-04 16:29:12 +0100

    fix: typo

--- src/features/ui/compactView/index.js
+++ src/features/ui/compactView/index.js
@@ -1,3 +1,3 @@
 export { CompactViewProvider } from "./contexts/CompactViewContext";
 export { default as useCompactView } from "./hooks/useCompactView";
-export { default as CompactViewBtn } from "./components/CompactViewBtn";
+export { default as CompactViewBtn } from "./components/compactViewBtn";
